d5e1ac115860199262ee5f630705f02398e8f395
Vinoaj Vijeyakumaar <vinoaj@gmail.com>
2018-03-07 18:02:49 +1100

    hubspot.js

---
+++ hubspot.js
@@ -0,0 +1,20 @@
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+<script>
+    hbspt.forms.create({
+        portalId: "4367112",
+        formId: "22ebfa6a-3071-47d9-9978-8828d4d354ce",
+        onFormReady: function($form) {
+            initForm();
+        }
+    });
+    function initForm() {
+        console.log('form loaded 2');
+        var inputs = document.getElementsByTagName('input');
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].addEventListener('click', recordInteraction, false);
+        }
+    }
+    function recordInteraction(event) {
+        console.log("Field name: ", this.name);
+    }
+</script>
